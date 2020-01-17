efa7f93b801cf0134332052b2ddbd192a17b4496
Ceri Thomas <ct229252@falmouth.ac.uk>
2020-01-17 14:48:10 +0000

    Created a key prefab and started the script for picking up the keys.

---
+++ Torcsvar_Unity/Assets/Scripts/ObjectPickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPickup : MonoBehaviour
+{
+    //When player is near object, player presses a button to pick up.
+
+    public GameObject Key;
+
+    private bool boolKey = false;
+
+    void start()
+    {
+        pressButton();
+    }
+
+    //picking up the item after pressing Q.
+    public void pressButton()
+    {
+        Debug.Log("Enters function");
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (Key.CompareTag("Key"))
+            {
+                Debug.Log("Picked up key");
+                boolKey = true;
+            }
+        }
+       // else
+        ///{
+          //  Debug.Log("Object not picked up");
+       // }
+    }
+}
