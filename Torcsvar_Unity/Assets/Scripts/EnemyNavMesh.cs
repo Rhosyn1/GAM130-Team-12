df564a95278304602dbb7c37bfb20d5486d7abb4
Rhosyn1 <ct229252@falmouth.ac.uk>
2020-02-01 12:26:10 +0000

    Created a basic enemy patrol path.

--- Torcsvar_Unity/Assets/Scripts/EnemyNavMesh.cs
+++ Torcsvar_Unity/Assets/Scripts/EnemyNavMesh.cs
@@ -7,15 +7,50 @@ public class EnemyNavMesh : MonoBehaviour
 {
     public Transform target;
     private NavMeshAgent agent;
+
+    public EnemyPatrolPath patrolPath;
+
+    private EnemyPatrolPath currentPatrol;
+    private int currentPoint;
+
     // Start is called before the first frame update
     void Start()
     {
+        currentPatrol = new EnemyPatrolPath(new List<Vector3>()
+        {
+            new Vector3(20, 1, 44),
+            new Vector3(20, 1, -1),
+            new Vector3(-9, 1, -1),
+            new Vector3(-9, 1, 44)
+        });
+
         agent = GetComponent<NavMeshAgent>();
+        if (patrolPath != null)
+        {
+            currentPatrol = patrolPath;
+            currentPoint = 0;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        if (currentPatrol != null)
+        {
+            agent.SetDestination(currentPatrol.patrolPoints[currentPoint]);
+
+            if (Vector3.Distance(transform.position, currentPatrol.patrolPoints[currentPoint]) <= 2f)
+            {
+                currentPoint++;
+                if (currentPoint > currentPatrol.patrolPoints.Count - 1)
+                {
+                    currentPoint = 0;
+                }
+            }
+        }
+        else
+        {
+            agent.SetDestination(target.position);
+        }
     }
 }
---
+++ Torcsvar_Unity/Assets/Scripts/EnemyPatrolPath.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolPath
+{
+    public List<Vector3> patrolPoints;
+
+    public EnemyPatrolPath(List<Vector3> path)
+    {
+        patrolPoints = path;
+    }
+}
