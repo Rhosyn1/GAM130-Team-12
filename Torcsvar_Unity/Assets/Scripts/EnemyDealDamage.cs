122c7f306ff20a3f10dfe30a150727ee6b8aa924
Ceri <ct229252@falmouth.ac.uk>
2020-02-04 16:15:59 +0000

    Enemy now damages player and player can die.

---
+++ Torcsvar_Unity/Assets/Scripts/EnemyDealDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyDealDamage : MonoBehaviour
+{
+    public int playerHealth;
+    public GameObject playerObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, playerObject.transform.position) <= 2f)
+        {
+            playerHealth--;
+            if (playerHealth <= 0)
+            {
+                SceneManager.LoadScene(2);
+            }
+        }
+    }
+}
