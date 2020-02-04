0c4e0e73dadf34de7c41a3f82ae69888f929bdf6
Ceri <ct229252@falmouth.ac.uk>
2020-02-04 15:58:10 +0000

    Made a basic title scene with a play button.

---
+++ Torcsvar_Unity/Assets/Scripts/playButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class playButton : MonoBehaviour
+{
+  
+    public void OnClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+  
+}
