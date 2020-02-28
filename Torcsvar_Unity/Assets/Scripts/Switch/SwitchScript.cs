95f00f32f190bd214ebfa4b6236a08745b5fb456
JD233113 <JD233113@falmouth.ac.uk>
2020-02-28 10:53:26 +0000

    started implementation of doors connected to switches

---
+++ Torcsvar_Unity/Assets/Scripts/Switch/SwitchScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour
+{
+    public GameObject door1;
+    public GameObject door2;
+
+    public void Use()
+    {
+
+    }
+}
---
+++ Torcsvar_Unity/Assets/Scripts/Switch/UseSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UseSwitch : MonoBehaviour
+{
+    public Text switchText;
+    public Camera playerCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Use a raycast to see if the player is looking at a switch or not
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, 2.5f))
+        {
+            if (hit.collider.gameObject.name == "Switch")
+            {
+                switchText.gameObject.SetActive(true);
+            }
+            else
+            {
+                switchText.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            switchText.gameObject.SetActive(false);
+        }
+    }
+}
