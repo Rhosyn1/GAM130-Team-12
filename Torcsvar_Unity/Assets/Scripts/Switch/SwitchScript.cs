2d287fa2df7822ac4992e2a240a8ffca34a2c51c
JD233113 <JD233113@falmouth.ac.uk>
2020-02-28 11:26:27 +0000

    merged window breaking and switch use into a single script to improve performance slightly

--- Torcsvar_Unity/Assets/Scripts/Switch/SwitchScript.cs
+++ Torcsvar_Unity/Assets/Scripts/Switch/SwitchScript.cs
@@ -6,9 +6,10 @@ public class SwitchScript : MonoBehaviour
 {
     public GameObject door1;
     public GameObject door2;
+    bool door1Open = false;
 
     public void Use()
     {
-
+        
     }
 }
